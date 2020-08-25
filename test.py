7cdf173b0fad982dad1eafbec5b90c0265a325f7
Chris Harris <62467247+HarrisPy@users.noreply.github.com>
2020-08-25 14:22:54 +0100

    Add files via upload
    Import and execution of new function on randomly selected directory

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 12:11:20 2020
+
+@author: charris
+"""
+
+from df_labeller import autosetup
+
+autosetup('C:/Users/chris/Documents/datasets_xfer/epl2020/')
+
