d8125d418ba7870e2c05849d54286bd604eaefcd
Chris Harris <62467247+HarrisPy@users.noreply.github.com>
2020-08-25 14:18:35 +0100

    Add files via upload
    function: autosetup imported and used on random directory

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 12:11:20 2020
+
+@author: charris
+"""
+
+from df_labeller import autosetup
+
+autosetup('C:/Users/chris/Documents/datasets_xfer/epl2020/')
+
+# working directory needs to = location of df_labeller.py
+# df_labeller need to have been run in kernal for this to work...??
